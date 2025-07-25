96017671092333c7bea0ed208a9ad02e210b6562
DolarumRunobux <mariaterezaferreirapires38@gmail.com>
2025-07-25 20:35:10 -0300

    Delete MARIA CAKES directory

--- MARIA CAKES/main.js
+++
@@ -1,6 +0,0 @@
-
-import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom";
-import MariaCakesApp from "./mariaCakesApp.js";
-
-ReactDOM.render(React.createElement(MariaCakesApp), document.getElementById("root"));
