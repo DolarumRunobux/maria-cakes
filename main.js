2bd9557ca16575352f54b041b3f9fce652ebaacb
DolarumRunobux <mariaterezaferreirapires38@gmail.com>
2025-07-25 20:34:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+import MariaCakesApp from "./mariaCakesApp.js";
+
+ReactDOM.render(React.createElement(MariaCakesApp), document.getElementById("root"));
